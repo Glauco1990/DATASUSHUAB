18aab76f065dbe40824f7c34d41fccea6e6b3da9
Glauco1990 <glaucoestatistico@gmail.com>
2020-08-23 23:47:49 -0300

    tentando fazer um loop para gerar os arquivos rda

--- data-raw/Compilar.R
+++ data-raw/Compilar.R
@@ -2,4 +2,34 @@
 
 PAHUAB1501 <- readr::read_rds("data-raw/PA/PAHUAB1501.rds")
 
-usethis::use_data(PAHUAB1501, overwrite = TRUE)
+# Ler varios arquivos
+arquivo <- list.files("data-raw/RDS", pattern = ".rds")
+caminho <- NULL
+saida <- NULL
+a <- NULL
+b <- NULL
+for (i in 1 : length(arquivo))
+{
+  caminho[i] <- stringr::str_c("data-raw/RDS/",arquivo[i])
+  saida[i] <- stringr::str_c(stringr::str_sub(arquivo[i], end = 10))
+}
+
+dados <- purrr::map(caminho, readr::read_rds)
+
+for (i in 1 : length(arquivo))
+{
+
+ names(dados[i]) <- saida[i]
+}
+
+
+for (i in 1 : length(arquivo))
+{
+
+  usethis::use_data(dados[[i]], overwrite = TRUE)
+}
+
+
+
+
+
